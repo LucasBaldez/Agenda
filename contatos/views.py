4894f50c56c38c80af404fd1032336a1c6f43793
Lucas Rodrigues Baldez <lucas.baldez@corp.medgrupo.net>
2020-11-20 09:25:03 -0300

    cricação da função de pesquisa

--- contatos/views.py
+++ contatos/views.py
@@ -3,7 +3,14 @@ from .models import Contato
 
 
 def index(request):
-    contatos = Contato.objects.all()
-    return render(request, 'contatos/index.html', {
+    if request.method=='POST':
+        contatos_nome = request.POST.get('nome')
+        search_contatos = Contato.objects.filter(nome__icontains = contatos_nome)
+        return render(request, 'contatos/index.html', {
+            'contatos': search_contatos
+        })
+    else:
+        contatos = Contato.objects.all()
+        return render(request, 'contatos/index.html', {
         'contatos': contatos
-    })
+        })
